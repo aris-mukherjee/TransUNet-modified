6efb8950d5cf6adbde626940b7fc399bcdb7d7ad
Aris Mukherjee <arismu@student.ethz.ch>
2021-11-15 23:08:34 +0100

    Code adapted for testing (poor performance)

--- test.py
+++ test.py
@@ -13,55 +13,100 @@ from datasets.dataset_synapse import Synapse_dataset
 from utils import test_single_volume
 from networks.vit_seg_modeling import VisionTransformer as ViT_seg
 from networks.vit_seg_modeling import CONFIGS as CONFIGS_ViT_seg
+import config.system_paths as sys_config
+import utils_data
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--volume_path', type=str,
-                    default='../data/Synapse/test_vol_h5', help='root dir for validation volume data')  # for acdc volume_path=root_dir
-parser.add_argument('--dataset', type=str,
-                    default='Synapse', help='experiment_name')
+                    default='/itet-stor/arismu/bmicdatasets-originals/Originals/Challenge_Datasets/NCI_Prostate/', help='root dir for validation volume data')  # for acdc volume_path=root_dir
+parser.add_argument('--test_dataset', type=str,
+                    default='RUNMC', help='experiment_name')
 parser.add_argument('--num_classes', type=int,
-                    default=4, help='output channel of network')
-parser.add_argument('--list_dir', type=str,
-                    default='./lists/lists_Synapse', help='list dir')
+                    default=3, help='output channel of network')
+#parser.add_argument('--list_dir', type=str,
+#                    default='./lists/lists_Synapse', help='list dir')
 
-parser.add_argument('--max_iterations', type=int,default=30000, help='maximum epoch number to train')
+parser.add_argument('--max_iterations', type=int,default=30001, help='maximum epoch number to train')
 parser.add_argument('--max_epochs', type=int, default=150, help='maximum epoch number to train')
-parser.add_argument('--batch_size', type=int, default=24,
+parser.add_argument('--batch_size', type=int, default=16,
                     help='batch_size per gpu')
-parser.add_argument('--img_size', type=int, default=224, help='input patch size of network input')
-parser.add_argument('--is_savenii', action="store_true", help='whether to save results during inference')
+parser.add_argument('--img_size', type=int, default=256, help='input patch size of network input')
+#parser.add_argument('--is_savenii', action="store_true", help='whether to save results during inference')
+parser.add_argument('--is_savenii', type=bool, default=True, help='whether to save results during inference')
 
 parser.add_argument('--n_skip', type=int, default=3, help='using number of skip-connect, default is num')
-parser.add_argument('--vit_name', type=str, default='ViT-B_16', help='select one vit model')
+parser.add_argument('--vit_name', type=str, default='R50-ViT-B_16', help='select one vit model')
 
 parser.add_argument('--test_save_dir', type=str, default='../predictions', help='saving prediction as nii!')
 parser.add_argument('--deterministic', type=int,  default=1, help='whether use deterministic training')
-parser.add_argument('--base_lr', type=float,  default=0.01, help='segmentation network learning rate')
+parser.add_argument('--base_lr', type=float,  default=1e-3, help='segmentation network learning rate')
 parser.add_argument('--seed', type=int, default=1234, help='random seed')
 parser.add_argument('--vit_patches_size', type=int, default=16, help='vit_patches_size, default is 16')
+parser.add_argument('--target_resolution', type=float, default=0.625, help='target resolution')   
+parser.add_argument('--image_depth_tr', type=int, default=32, help='target resolution') 
+parser.add_argument('--image_depth_ts', type=int, default=32, help='target resolution')    
+parser.add_argument('--test_cv_fold_num', type = int, default = 1) # 1 / 2 / 3 / 4
+parser.add_argument('--NORMALIZE', type = int, default = 1) # 1 / 0
 args = parser.parse_args()
 
 
+
 def inference(args, model, test_save_path=None):
-    db_test = args.Dataset(base_dir=args.volume_path, split="test_vol", list_dir=args.list_dir)
-    testloader = DataLoader(db_test, batch_size=1, shuffle=False, num_workers=1)
-    logging.info("{} test iterations per epoch".format(len(testloader)))
+    
+    loaded_test_data = utils_data.load_testing_data(args.test_dataset,  #needs to be adapted for different test set
+                                                    args.test_cv_fold_num,
+                                                    args.img_size,
+                                                    args.target_resolution,
+                                                    args.image_depth_ts)
+
+
+    imts = loaded_test_data[0]
+    gtts = loaded_test_data[1]
+    orig_data_res_x = loaded_test_data[2]
+    orig_data_res_y = loaded_test_data[3]
+    orig_data_res_z = loaded_test_data[4]
+    orig_data_siz_x = loaded_test_data[5]
+    orig_data_siz_y = loaded_test_data[6]
+    orig_data_siz_z = loaded_test_data[7]
+    name_test_subjects = loaded_test_data[8]
+    num_test_subjects = loaded_test_data[9]
+    ids = loaded_test_data[10]
+
+    images = []
+    labels = []
+
     model.eval()
     metric_list = 0.0
-    for i_batch, sampled_batch in tqdm(enumerate(testloader)):
-        h, w = sampled_batch["image"].size()[2:]
-        image, label, case_name = sampled_batch["image"], sampled_batch["label"], sampled_batch['case_name'][0]
+
+    for sub_num in range(num_test_subjects):
+
+        subject_id_start_slice = np.sum(orig_data_siz_z[:sub_num])
+        subject_id_end_slice = np.sum(orig_data_siz_z[:sub_num+1])
+        image = imts[subject_id_start_slice:subject_id_end_slice,:,:] 
+        label = gtts[subject_id_start_slice:subject_id_end_slice,:,:] 
+        print(label[10])
+
+        # ==================================================================
+        # setup logging
+        # ==================================================================
+        logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
+        subject_name = str(name_test_subjects[sub_num])[2:-1]
+        logging.info('============================================================')
+        logging.info('Subject ' + str(sub_num) + ' out of ' + str(num_test_subjects) + ': ' + subject_name)
+
+
         metric_i = test_single_volume(image, label, model, classes=args.num_classes, patch_size=[args.img_size, args.img_size],
-                                      test_save_path=test_save_path, case=case_name, z_spacing=args.z_spacing)
+                                      test_save_path=test_save_path, case=sub_num, z_spacing=args.z_spacing)
+
         metric_list += np.array(metric_i)
-        logging.info('idx %d case %s mean_dice %f mean_hd95 %f' % (i_batch, case_name, np.mean(metric_i, axis=0)[0], np.mean(metric_i, axis=0)[1]))
-    metric_list = metric_list / len(db_test)
-    for i in range(1, args.num_classes):
-        logging.info('Mean class %d mean_dice %f mean_hd95 %f' % (i, metric_list[i-1][0], metric_list[i-1][1]))
-    performance = np.mean(metric_list, axis=0)[0]
-    mean_hd95 = np.mean(metric_list, axis=0)[1]
-    logging.info('Testing performance in best val model: mean_dice : %f mean_hd95 : %f' % (performance, mean_hd95))
-    return "Testing Finished!"
+        logging.info('case %s mean_dice %f mean_hd95 %f' % (sub_num, np.mean(metric_i, axis=0)[0], np.mean(metric_i, axis=0)[1]))
+        metric_list = metric_list / imts.shape[0]
+        for i in range(1, args.num_classes):
+            logging.info('Mean class %d mean_dice %f mean_hd95 %f' % (i, metric_list[i-1][0], metric_list[i-1][1]))
+        performance = np.mean(metric_list, axis=0)[0]
+        mean_hd95 = np.mean(metric_list, axis=0)[1]
+        logging.info('Testing performance in best val model: mean_dice : %f mean_hd95 : %f' % (performance, mean_hd95))
+        return "Testing Finished!"
 
 
 if __name__ == "__main__":
@@ -78,19 +123,18 @@ if __name__ == "__main__":
     torch.cuda.manual_seed(args.seed)
 
     dataset_config = {
-        'Synapse': {
-            'Dataset': Synapse_dataset,
-            'volume_path': '../data/Synapse/test_vol_h5',
-            'list_dir': './lists/lists_Synapse',
-            'num_classes': 9,
+        'RUNMC': {
+            'volume_path': '/itet-stor/arismu/bmicdatasets-originals/Originals/Challenge_Datasets/NCI_Prostate/',
+            #'list_dir': './lists/lists_Synapse',
+            'num_classes': 3,
             'z_spacing': 1,
         },
     }
-    dataset_name = args.dataset
+    dataset_name = args.test_dataset
     args.num_classes = dataset_config[dataset_name]['num_classes']
     args.volume_path = dataset_config[dataset_name]['volume_path']
-    args.Dataset = dataset_config[dataset_name]['Dataset']
-    args.list_dir = dataset_config[dataset_name]['list_dir']
+    args.Dataset = dataset_name
+    #args.list_dir = dataset_config[dataset_name]['list_dir']
     args.z_spacing = dataset_config[dataset_name]['z_spacing']
     args.is_pretrain = True
 
@@ -101,11 +145,12 @@ if __name__ == "__main__":
     snapshot_path += '_' + args.vit_name
     snapshot_path = snapshot_path + '_skip' + str(args.n_skip)
     snapshot_path = snapshot_path + '_vitpatch' + str(args.vit_patches_size) if args.vit_patches_size!=16 else snapshot_path
-    snapshot_path = snapshot_path + '_epo' + str(args.max_epochs) 
+    snapshot_path = snapshot_path+'_'+str(args.max_iterations)[0:2]+'k' if args.max_iterations != 30001 else snapshot_path
+    snapshot_path = snapshot_path + '_epo' +str(args.max_epochs) if args.max_epochs != 150 else snapshot_path
     if dataset_name == 'ACDC':  # using max_epoch instead of iteration to control training duration
         snapshot_path = snapshot_path + '_' + str(args.max_iterations)[0:2] + 'k' if args.max_iterations != 30000 else snapshot_path
     snapshot_path = snapshot_path+'_bs'+str(args.batch_size)
-    snapshot_path = snapshot_path + '_lr' + str(args.base_lr) if args.base_lr != 0.01 else snapshot_path
+    snapshot_path = snapshot_path + '_lr' + str(args.base_lr) if args.base_lr != 1e-3 else snapshot_path
     snapshot_path = snapshot_path + '_'+str(args.img_size)
     snapshot_path = snapshot_path + '_s'+str(args.seed) if args.seed!=1234 else snapshot_path
 
@@ -115,11 +160,11 @@ if __name__ == "__main__":
     config_vit.patches.size = (args.vit_patches_size, args.vit_patches_size)
     if args.vit_name.find('R50') !=-1:
         config_vit.patches.grid = (int(args.img_size/args.vit_patches_size), int(args.img_size/args.vit_patches_size))
-    net = ViT_seg(config_vit, img_size=args.img_size, num_classes=config_vit.n_classes).cuda()
+    net = ViT_seg(config_vit, img_size=args.img_size, num_classes=config_vit.n_classes)#.cuda()
 
     snapshot = os.path.join(snapshot_path, 'best_model.pth')
     if not os.path.exists(snapshot): snapshot = snapshot.replace('best_model', 'epoch_'+str(args.max_epochs-1))
-    net.load_state_dict(torch.load(snapshot))
+    #net.load_state_dict(torch.load(snapshot))
     snapshot_name = snapshot_path.split('/')[-1]
 
     log_folder = './test_log/test_log_' + args.exp
